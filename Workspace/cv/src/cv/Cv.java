ebbf7de507c14e3069af38cbc7fb4272701e0021
201768215 <allowa2000@hotmail.fr>
2019-02-03 23:16:34 -0500

    Le code terminer

--- Workspace/cv/src/cv/Cv.java
+++ Workspace/cv/src/cv/Cv.java
@@ -1,5 +1,7 @@
 package cv;
 
+import java.util.Arrays;
+
 public class Cv {
 
 	private static String nom;
@@ -11,51 +13,57 @@ public class Cv {
 	
 	public static void main(String[] args) {
 		
+		String competencesAI[] = new String[] {"Perséverant", "Travaillant quand il s'y met"};
+		Cv andrewI = new Cv("Andrew","Idehen", "Technique d'informatique de gestion", 2, competencesAI, "Maitriser Git d'ici la fin de la session");
+		affiche(andrewI);
 		
+		System.out.println();
+		
+		String competencesAA[] = new String[] {"Apprend facilement", "Créatif"};
+		Cv andrewA = new Cv("Andrew","Abou Antoun", "Technique d'informatique de gestion", 5, competencesAA, "Maitriser Git d'ici la fin de la session");
+		affiche(andrewA);
 	}
-
-	public void setNom(String nom) {
-		nom = this.nom;
+	
+	public static void affiche (Cv personne) {
+		
+		String affichage ="Bienvenue chez Barette!" + 
+				"\nNom: " + personne.getNom()
+				+ "\n"+ "Prenom: " + personne.getPrenom()
+				+ "\n"+ "Formation: " + personne.getFormation()
+				+ "\n"+ "Expérience de travail (nombre d'années): " + personne.getExperience()
+				+ "\n"+ "Competences: " + Arrays.toString(personne.getCompetences())
+				+ "\n"+ "Attentes vis à vis le cours 4B4: " + personne.getAttentes();
+		
+		System.out.println(affichage);
+	} 
+	
+	public Cv(String nom, String prenom, String formation, int experience, String [] competences, String attentes) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.experience = experience;
+		this.competences = competences;
+		this.attentes = attentes;
 	}
 	
 	public static String getNom() {
 		return nom;
 	}
 	
-	public void setPrenom(String prenom) {
-		prenom = this.prenom;
-	}
-	
 	public static String getPrenom() {
 		return prenom;
-	}
-	
-	public void setFormation(String formation) {
-		formation = this.formation;
 	}
 	
 	public static String getFormation() {
 		return formation;
 	}
 	
-	public void setExperience(int experience) {
-		experience = this.experience;
-	}
-	
 	public static int getExperience() {
 		return experience;
 	}
 	
-	public void setCompetences(String[] competences) {
-		competences = this.competences;
-	}
-	
 	public static String[] getCompetences() {
 		return competences;
-	}
-	
-	public void setAttentes(String attentes) {
-		attentes = this.attentes;
 	}
 	
 	public static String getAttentes() {
